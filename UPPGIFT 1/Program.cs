540df9cf56eb51b18f4cf5d174048901df900f34
kual04001 <kual04001@E-PW0234KG>
2023-09-15 13:06:15 +0200

    Add project files.

---
+++ UPPGIFT 1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Du är nestan klar med kapitel 1");
+Console.WriteLine("Du har lärt dig skriva ut tecken som &quot; och \\.");
+
+
